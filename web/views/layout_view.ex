1ade8d79b82380c451d17f3370976d651e34a1cd
Cassio Godinho <cassiopgodinho@gmail.com>
2016-11-25 17:23:26 -0200

    current user helper

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule Authen.LayoutView do
   use Authen.Web, :view
+
+  def current_user(%{assigns: %{current_user: user}}) do
+    user
+  end
+
+  def current_user(nil) do
+    false
+  end
+
 end
